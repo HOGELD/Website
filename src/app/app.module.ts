5a60c7ce26c80e3b9d2d76ff825797150cc5e456
sharbel93 <sharbel.cosa@gmail.com>
2020-06-10 15:29:55 +0300

    edit footer and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HomeModule} from './modules/pages/home/home.module';
+import {HeaderModule} from "./core/header/header.module";
+import {FooterModule} from "./core/footer/footer.module";
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HomeModule,
-    BrowserAnimationsModule
+    HeaderModule,
+    BrowserAnimationsModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
